2353c43bfdbcb7a0011c6287669f89e0fd82ff75
patrick0422 <didxodnd0422@gmail.com>
2021-03-09 11:41:48 +0900

    즐거운 앱프시간

--- SchoolWork/app/src/main/java/com/example/app01/MainActivity.java
+++ SchoolWork/app/src/main/java/com/example/app01/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.app01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv3 = findViewById(R.id.tv3);
+        tv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv3.setText(tv3.getText() + " 문자추가");
+                tv3.setTextColor(Color.RED);
+                tv3.setTextSize(30);
+            }
+        });
     }
 }
