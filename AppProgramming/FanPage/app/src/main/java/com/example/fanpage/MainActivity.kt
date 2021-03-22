cac0792ae42570a084c037f366f3fab1f483958d
patrick0422 <didxodnd0422@gmail.com>
2021-03-22 21:18:58 +0900

    스플래시 화면 생성

--- AppProgramming/FanPage/app/src/main/java/com/example/fanpage/MainActivity.kt
+++ AppProgramming/FanPage/app/src/main/java/com/example/fanpage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fanpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.fanpage.databinding.ActivityMainBinding
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        val intent = Intent(this, SplashActivity::class.java)
 
+        binding.btn1.setOnClickListener { startActivity(intent) }
     }
 }
---
+++ AppProgramming/FanPage/app/src/main/java/com/example/fanpage/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.fanpage
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+}
